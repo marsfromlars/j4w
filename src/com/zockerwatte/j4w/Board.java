53d8d09b2df989b8eeae2008b7b02fe56cd9da26
Lars Löffler <lars.loeffler@infodesire.com>
2020-08-03 16:45:26 +0200

    save to String or File and load

--- src/com/zockerwatte/j4w/Board.java
+++ src/com/zockerwatte/j4w/Board.java
@@ -1,6 +1,9 @@
 package com.zockerwatte.j4w;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -20,6 +23,32 @@ public class Board {
   private Board( List<List<Field>> columns ) {
     this.columns = columns;
     checkForWinner();
+  }
+
+  public static Board load( File file ) throws IOException {
+    BufferedReader in = new BufferedReader( new FileReader( file ) );
+    StringBuilder data = new StringBuilder();
+    String line = in.readLine();
+    while( line != null ) {
+      data.append( line + "\n" );
+      line = in.readLine();
+    }
+    return load( data.toString() );
+  }
+
+  public static Board load( String data ) {
+    Board board = new Board();
+    List<String> lines = Arrays.stream( data.split( "\n" ) ).collect( Collectors.toList() );
+    Collections.reverse( lines );
+    for( int row = 0; row < 6; row++ ) {
+      String line = lines.get( row );
+      for( int column = 0; column < 7; column++ ) {
+        char c = line.charAt( column );
+        Field.Status status = c == 'r' ? Field.Status.RED : c == 'b' ? Field.Status.BLUE : Field.Status.EMPTY;
+        board.columns.get( column ).set( row, new Field( status ) );
+      }
+    }
+    return board;
   }
 
   private void checkForWinner() {
@@ -132,13 +161,13 @@ public class Board {
 
     return new Board(
       IntStream.range( 0, 7 )
-        .mapToObj( i -> dropIfColumn( dropColumnIndex, i, isRed ) )
+        .mapToObj( i -> dropIfColumnIndexIsDropIndex( dropColumnIndex, i, isRed ) )
         .collect( Collectors.toList() )
     );
 
   }
 
-  private List<Field> dropIfColumn( int dropIndex, int columnIndex, boolean isRed ) {
+  private List<Field> dropIfColumnIndexIsDropIndex( int dropIndex, int columnIndex, boolean isRed ) {
     if( dropIndex == columnIndex ) {
       return dropInColumn( dropIndex, isRed );
     }
@@ -158,7 +187,10 @@ public class Board {
       throw new ArrayIndexOutOfBoundsException( "Column " + dropIndex + " already full." );
     }
 
+    // the new field
     newColumn.add( new Field( isRed ? Field.Status.RED : Field.Status.BLUE ) );
+
+    // fill the rest with empty
     while( newColumn.size() < 6 ) {
       newColumn.add( new Field( Field.Status.EMPTY ) );
     }
@@ -195,6 +227,43 @@ public class Board {
 
   public boolean winnerIsBlue() {
     return !winnerIsRed();
+  }
+
+  public int getColumnHeight( int columnIndex ) {
+    return (int) columns.get( columnIndex ).stream().filter( field -> !field.isEmpty() ).count();
+  }
+
+  public boolean equals( Object other ) {
+    if( other.getClass() != Board.class ) {
+      return false;
+    }
+    Board otherBoard = (Board) other;
+    if( hasWinner != otherBoard.hasWinner ) {
+      return false;
+    }
+    if( winner != otherBoard.winner ) {
+      return false;
+    }
+    return IntStream.range( 0, 7 )
+      .filter( i -> columnEqual( otherBoard, i ) )
+      .count() == 7;
+  }
+
+  private boolean columnEqual( Board otherBoard, int columnIndex ) {
+
+    List<Field> thisColumn = columns.get( columnIndex );
+    List<Field> otherColumn = columns.get( columnIndex );
+
+    return IntStream.range( 0, 6 )
+      .filter( i -> thisColumn.get( i ).getStatus() == thisColumn.get( i ).getStatus() )
+      .count() == 6;
+
+  }
+
+  public void save( File file ) throws FileNotFoundException {
+    PrintWriter out = new PrintWriter( file );
+    out.println( toString() );
+    out.close();
   }
 
 }
--- tests/com/zockerwatte/j4w/BoardTest.java
+++ tests/com/zockerwatte/j4w/BoardTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class BoardTest {
@@ -51,7 +54,18 @@ public class BoardTest {
     assertFalse( new Board().drop( 0, true ).isEmpty() );
   }
 
-  @Test(expected = ArrayIndexOutOfBoundsException.class)
+  @Test
+  public void dropIncreasesColumnHeight() {
+
+    Board board = new Board();
+
+    assertEquals( 0, board.getColumnHeight( 3 ) );
+    assertEquals( 1, board.drop( 3, true ).getColumnHeight( 3 ) );
+    assertEquals( 4, board.drop( 3, true ).drop( 3, true ).drop( 3, true ).drop( 3, true ).getColumnHeight( 3 ) );
+
+  }
+
+  @Test( expected = ArrayIndexOutOfBoundsException.class )
   public void throwsExceptionWhenFull() {
     new Board()
       .drop( 0, true )
@@ -119,8 +133,11 @@ public class BoardTest {
       .drop( 3, false )
       .drop( 3, true )
       .drop( 3, true )
-      .drop( 3, true )
       .drop( 3, true );
+
+    assertFalse( board.hasWinner() );
+
+      board = board.drop( 3, true );
 
     assertTrue( board.hasWinner() && board.winnerIsRed() );
 
@@ -136,8 +153,11 @@ public class BoardTest {
       .drop( 4, true )
       .drop( 4, false )
       .drop( 3, false )
-      .drop( 2, false )
-      .drop( 1, false );
+      .drop( 2, false );
+
+    assertFalse( board.hasWinner() );
+
+      board = board.drop( 1, false );
 
     assertTrue( board.hasWinner() && board.winnerIsBlue() );
 
@@ -155,8 +175,11 @@ public class BoardTest {
       .drop( 3, true )
       .drop( 4, false )
       .drop( 3, false )
-      .drop( 4, false )
       .drop( 4, false );
+
+    assertFalse( board.hasWinner() );
+
+      board = board.drop( 4, false );
 
     assertTrue( board.hasWinner() && board.winnerIsBlue() );
 
@@ -172,6 +195,118 @@ public class BoardTest {
       .drop( 3, true )
       .drop( 3, true )
       .drop( 3, true );
+
+  }
+
+  @Test
+  public void equalDropsIsEqual() {
+
+    Board b1 = new Board();
+    Board b2 = new Board();
+
+    b1.drop( 2, false );
+    b1.drop( 1, true );
+    b1.drop( 3, false );
+    b1.drop( 3, true );
+    b1.drop( 4, true );
+    b1.drop( 5, false );
+    b1.drop( 1, true );
+    b1.drop( 2, false );
+    b1.drop( 6, false );
+
+    b2.drop( 2, false );
+    b2.drop( 1, true );
+    b2.drop( 3, false );
+    b2.drop( 3, true );
+    b2.drop( 4, true );
+    b2.drop( 5, false );
+    b2.drop( 1, true );
+    b2.drop( 2, false );
+    b2.drop( 6, false );
+
+    assertTrue( b1.equals( b2 ) && b2.equals( b1 ) );
+
+  }
+
+  @Test
+  public void saveToFileAndLoadAgainIsEqual() throws IOException {
+
+    Board board = new Board()
+      .drop( 0, true )
+      .drop( 1, false )
+      .drop( 1, true )
+      .drop( 1, true )
+      .drop( 3, false )
+      .drop( 4, true )
+      .drop( 4, true )
+      .drop( 5, true )
+      .drop( 6, false );
+
+    File file = File.createTempFile( "f4w-board-test-", ".txt" );
+    try {
+
+      board.save( file );
+      Board board2 = Board.load( file );
+
+      assertEquals( board, board2 );
+
+    }
+    finally {
+      file.delete();
+    }
+
+  }
+
+
+  @Test
+  public void initializeFromStringJustWorksFine_empty() {
+
+    Board emptyBoard = Board.load(
+        ".......\n"
+      + ".......\n"
+      + ".......\n"
+      + ".......\n"
+      + ".......\n"
+      + ".......\n"
+    );
+
+    assertTrue( emptyBoard.isEmpty() );
+
+  }
+
+  @Test
+  public void initializeFromStringJustWorksFine() {
+
+    Board board = Board.load(
+          ".......\n"
+        + "......r\n"
+        + ".r....b\n"
+        + ".r....b\n"
+        + ".bbb..r\n"
+        + ".rbrr.b\n"
+    );
+
+    assertTrue( board.get( 1, 3 ).isRed() );
+    assertTrue( board.get( 1, 2 ).isRed() );
+    assertTrue( board.get( 1, 1 ).isBlue() );
+    assertTrue( board.get( 1, 0 ).isRed() );
+    assertTrue( board.get( 2, 1 ).isBlue() );
+    assertTrue( board.get( 2, 0 ).isBlue() );
+    assertTrue( board.get( 3, 1 ).isBlue() );
+    assertTrue( board.get( 3, 0 ).isRed() );
+    assertTrue( board.get( 4, 0 ).isRed() );
+    assertTrue( board.get( 6, 4 ).isRed() );
+    assertTrue( board.get( 6, 3 ).isBlue() );
+    assertTrue( board.get( 6, 2 ).isBlue() );
+    assertTrue( board.get( 6, 1 ).isRed() );
+    assertTrue( board.get( 6, 0 ).isBlue() );
+    assertTrue( board.getColumnHeight( 0 ) == 0 );
+    assertTrue( board.getColumnHeight( 1 ) == 4 );
+    assertTrue( board.getColumnHeight( 2 ) == 2 );
+    assertTrue( board.getColumnHeight( 3 ) == 2 );
+    assertTrue( board.getColumnHeight( 4 ) == 1 );
+    assertTrue( board.getColumnHeight( 5 ) == 0 );
+    assertTrue( board.getColumnHeight( 6 ) == 5 );
 
   }
 
